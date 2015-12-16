abbd6c92c5dfe591a4565e54b104b184af8c8c7e
Aaron <aschryver-cc@conestogac.on.ca>
2015-12-16 07:15:30 -0500

    Possible fix for production failure

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base 
-    attr_accessible :picture, :verified, :category, :tags, :user_id  
+    #attr_accessible :picture, :verified, :category, :tags, :user_id  
     
     validates :picture, :attachment_presence => true 
     
