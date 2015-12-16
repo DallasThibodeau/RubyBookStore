89ab1992a8a1a550d33867f460d6914158291dfb
Aaron <aschryver-cc@conestogac.on.ca>
2015-12-16 07:19:58 -0500

    Possible fix for production failure

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,24 +1,3 @@
 class Image < ActiveRecord::Base 
-    #attr_accessible :picture, :verified, :category, :tags, :user_id  
-    
-    validates :picture, :attachment_presence => true 
-    
-    #validates :tags, :presence =>true
-    
-    #validates :category, :presence => true    
-    
-    belongs_to :ad
-    
-    validates_attachment_size :picture, :less_than => 10.megabytes   
-    validates_attachment_presence :picture
-    
-    #added for paperclip-drop gem   
-    has_attached_file :picture,
-    :storage => :dropbox,
-    :dropbox_credentials => "#{Rails.root}/config/dropbox_config.yml",
-    :styles => { :medium => "300x300" , :thumb => "100x100>"},    
-    :dropbox_options => {
-      :path => proc { |style| "#{style}/#{id}_#{picture.original_filename}"},       
-      :unique_filename => true   
-    }
+
 end
